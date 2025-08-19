b78b73c81509b155fe67e2fe579154963d1c55fa
Michael-S-James <mjames1616@gmail.com>
2025-08-18 19:34:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const message = document.getElementById('message');
 button.addEventListener('click', () => {
   // Random fun fact
   const facts = [
-    "JavaScript can run in your browser!",
+    "JavaScript can run in your browser!!",
     "GitHub Pages hosts static websites for free.",
     "You just clicked a button using JavaScript!",
     "You can style this page with CSS.",
     "Hello from michael-s-james.github.io!"
   ],
-    window.open('https://example.com', '_blank')
+    window.open('https://drive.google.com/file/d/19fRzl4dsw88jKDm1K6iDS0CM2LPf87c2/view?usp=sharing', '_blank')
     ;
 
   const randomFact = facts[Math.floor(Math.random() * facts.length)];
