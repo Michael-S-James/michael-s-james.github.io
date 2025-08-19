25519333b8064ed70be0306450f62bf6ec3df50f
Michael-S-James <mjames1616@gmail.com>
2025-08-18 19:40:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const message = document.getElementById('message');
 button.addEventListener('click', () => {
   // Random fun fact
   const facts = [
-    "JavaScript can run in your browser!!",
+    "JavaScript can run in your browser!",
     "GitHub Pages hosts static websites for free.",
     "You just clicked a button using JavaScript!",
     "You can style this page with CSS.",
@@ -15,6 +15,9 @@ button.addEventListener('click', () => {
   message.textContent = randomFact;
 
   // Change background color
-  document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  window.open('https://drive.google.com/file/d/19fRzl4dsw88jKDm1K6iDS0CM2LPf87c2/view?usp=sharing', '_blank');
+  document.body.style.backgroundColor =
+    '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  // ✅ Open GitHub in a new tab
+  window.open('https://github.com/michael-s-james', '_blank');
 });
