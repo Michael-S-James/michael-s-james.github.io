fac8d55034c858151c603a0a6bad95c5bcc8bb91
Michael-S-James <mjames1616@gmail.com>
2025-08-18 19:10:26 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.getElementById('magicBtn');
+const message = document.getElementById('message');
+
+button.addEventListener('click', () => {
+  // Random fun fact
+  const facts = [
+    "JavaScript can run in your browser!",
+    "GitHub Pages hosts static websites for free.",
+    "You just clicked a button using JavaScript!",
+    "You can style this page with CSS.",
+    "Hello from michael-s-james.github.io!"
+  ];
+
+  const randomFact = facts[Math.floor(Math.random() * facts.length)];
+  message.textContent = randomFact;
+
+  // Change background color
+  document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+});
